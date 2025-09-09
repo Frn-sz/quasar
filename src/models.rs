505e9cf9ecd2d37c744fbaf80661a4050e5bca8c
Fernando Medeiros <fernandogdemedeirosw@gmail.com>
2025-09-09 19:49:17 -0300

    refactor: added initial models + code setup

---
+++ src/models.rs
@@ -0,0 +1,29 @@
+use uuid::Uuid;
+
+/// Represents a possible identifier for an account.
+#[derive(Debug, Clone)]
+pub enum Key {
+    CPF(String),
+    Email(String),
+    Phone(String),
+    Random(String),
+}
+
+/// Account is very simplified, since we don't really care about user data
+#[derive(Default, Debug, Clone)]
+pub struct Account {
+    pub uuid: uuid::Uuid,
+    // TODO: Implement a module to deal with integer
+    pub balance: i64,
+    pub keys: Vec<Key>,
+}
+
+impl Account {
+    pub fn new(keys: Vec<Key>) -> Self {
+        Account {
+            uuid: Uuid::new_v4(),
+            balance: 0,
+            keys,
+        }
+    }
+}
--- src/models/key.rs
+++
@@ -1,8 +0,0 @@
-// Some better way than String?
-pub enum Key {
-    CPF(String),
-    Email(String),
-    Phone(String),
-    // Random key
-    Generated(String),
-}
