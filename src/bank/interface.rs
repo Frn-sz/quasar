8182905e932cfbe918ecfde049d11ab604d11868
Fernando Medeiros <fernandogdemedeirosw@gmail.com>
2025-09-09 20:16:38 -0300

    wip: initial bank interface

---
+++ src/bank/interface.rs
@@ -0,0 +1,7 @@
+//! Bank is responsible for managing accounts and transactions in memory.
+//! It will load accounts and transactions from a persistent storage, if any.
+//! It will also save accounts and transactions to a persistent storage, if any.
+//! Both operations will only happen when the bank is started and stopped respectively.
+pub trait Bank {
+    // TODO: Define the bank interface
+}
