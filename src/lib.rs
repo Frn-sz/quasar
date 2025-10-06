852fbe8f125a2e89a24c5f2296e3cd78a7df8578
Fernando Medeiros <fernandogdemedeirosw@gmail.com>
2025-10-06 18:37:09 -0300

    wip: initial Quasar main service

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+use std::sync::{Arc, RwLock};
+
 pub mod ledger;
 pub mod models;
 pub mod transaction_processor;
+
+pub struct Quasar {
+    // Ledger has internal locking, so we can use it directly.
+    pub ledger: Arc<RwLock<ledger::Ledger>>,
+    // Same for TransactionProcessor.
+    pub transaction_processor: transaction_processor::TransactionProcessor,
+}
+
+impl Quasar {
+    pub fn new() -> Self {
+        let ledger = Arc::new(RwLock::new(ledger::Ledger::new()));
+
+        // Cheap clone of Arc
+        let transaction_processor =
+            transaction_processor::TransactionProcessor::new(ledger.clone());
+
+        Quasar {
+            ledger,
+            transaction_processor,
+        }
+    }
+
+    pub async fn run(&mut self) {}
+}
