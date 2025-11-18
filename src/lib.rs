fdb99ff4d0dde3c13c0c80ec5e723fd9e9679d70
Fernando Medeiros <fernandogdemedeirosw@gmail.com>
2025-11-18 08:46:32 -0300

    Add persistance (#16)
    * wip: persistence with SQLite
    
    * chore: add log + update config example

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 use {
-    crate::{grpc_server::start_grpc_service, logging::init_logging},
+    crate::{
+        grpc_server::start_grpc_service, ledger::Ledger, logging::init_logging,
+        persistence::Persistence, transaction_processor::TransactionProcessor,
+    },
     std::sync::{Arc, RwLock},
     tokio::signal::ctrl_c,
-    tracing::error,
+    tracing::{error, info},
 };
 
 pub mod config;
@@ -11,25 +14,34 @@ pub mod ledger;
 pub mod logging;
 pub mod metrics;
 pub mod models;
+pub mod persistence;
 pub mod transaction_processor;
 
 pub struct Quasar {
-    pub transaction_processor: Arc<RwLock<transaction_processor::TransactionProcessor>>,
+    pub transaction_processor: Arc<RwLock<TransactionProcessor>>,
     pub config: config::QuasarServerConfig,
+    pub persistence: Persistence,
+    ledger: Arc<RwLock<Ledger>>,
 }
 
 impl Quasar {
     pub fn new(config: config::QuasarServerConfig) -> Self {
-        let ledger = Arc::new(RwLock::new(ledger::Ledger::new()));
+        let persistence = Persistence::new(&config.persistence.db_path)
+            .expect("Failed to initialize persistence");
+        let accounts = persistence
+            .load_accounts()
+            .expect("Failed to load accounts");
+        let ledger = Arc::new(RwLock::new(Ledger::new(accounts)));
 
         // Cheap clone of Arc
-        let transaction_processor = Arc::new(RwLock::new(
-            transaction_processor::TransactionProcessor::new(ledger.clone()),
-        ));
+        let transaction_processor =
+            Arc::new(RwLock::new(TransactionProcessor::new(ledger.clone())));
 
         Quasar {
             transaction_processor,
             config,
+            persistence,
+            ledger,
         }
     }
 
@@ -39,6 +51,10 @@ impl Quasar {
         let _logging_guard = init_logging(self.config.debug);
 
         // TODO: add REST API service here
+        info!(
+            "Initializing with {} accounts",
+            self.ledger.read().unwrap().accounts.read().unwrap().len()
+        );
 
         {
             let grpc_processor = Arc::clone(&self.transaction_processor);
@@ -54,6 +70,11 @@ impl Quasar {
                 shutdown_sender.send(()).map_err(|e| e.to_string())?;
                 services.abort_all();
                 tracing::info!("Shutdown signal received, stopping services...");
+
+                let accounts = self.ledger.read().unwrap().accounts.read().unwrap().clone();
+                self.persistence.save_accounts(&accounts).expect("Failed to save accounts");
+
+                tracing::info!("Accounts saved successfully");
             }
             Some(res) = services.join_next() => {
                 error!("Error in task: {:?}", res);
